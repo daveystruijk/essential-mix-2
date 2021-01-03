8b55d25aa7d35490a01cdcec94d066c0b55b6481
Davey Struijk <mail@daveystruijk.com>
2021-01-03 13:33:36 +0100

    Add effects

--- essential_mix.py
+++ essential_mix.py
@@ -1,26 +1,26 @@
 import time
-from pydub import AudioSegment
+from pydub import AudioSegment, effects, scipy_effects
 from pydub.playback import play
-from track import Track
+from track import Track, change_tempo, apply_effect
 
 BPM = 128
 KEY_NOTATION = 'openkey'
 LIBRARY_DIR = "/Users/daveystruijk/Drive/Music"
 mix = AudioSegment.silent(duration=0)
 
-not_alone = Track(f"{LIBRARY_DIR}/Techno - Melodic/Time - Not Alone.mp3")
+not_alone = Track(f"{LIBRARY_DIR}/Techno - Melodic/Time - Not Alone.mp3", bpm=120.0)
 in_the_wild = Track(f"{LIBRARY_DIR}/Techno - Melodic/Dee Montero feat. Meliha - In The Wild.mp3")
 
-# output = sound1.overlay(sound2, position=5000)
+# mix += not_alone.bars(0, 32)
+not_alone_sync = apply_effect(change_tempo(not_alone.bars(160, 160+32), not_alone.bpm, in_the_wild.bpm), scipy_effects.high_pass_filter, 0.1, 10000)
+in_the_wild_sync = apply_effect(in_the_wild.bars(16, 16+32), scipy_effects.high_pass_filter, 10000, 0.1)
+mix += not_alone_sync.overlay(in_the_wild_sync)
+mix += in_the_wild.bars(48, 64)
+#output = sound1.overlay()
 
-mix += not_alone.beats(0, 4)
-mix += not_alone.beats(0, 4)
-mix += not_alone.beats(0, 4)
-mix += not_alone.beats(0, 4)
-mix += not_alone.beats(0, 4)
-mix += not_alone.beats(0, 4)
-
+#mix = effects.normalize(mix)
 play(mix)
+mix.export("./output.wav", format="wav")
 
 while True:
     time.sleep(1)
--- track.py
+++ track.py
@@ -4,21 +4,55 @@ import numpy as np
 import pyrubberband as pyrb
 from pydub import AudioSegment
 from pydub.silence import detect_leading_silence
+from pydub.utils import make_chunks
 from mutagen.mp3 import MP3
 from mutagen.id3 import TKEY, TBPM
+from easing_functions import QuadEaseIn
+
+
+def lerp(from_val, to_val, t):
+    return from_val * (1 - t) + to_val * t
+
+def apply_effect(audio, effect, from_val, to_val):
+    out = AudioSegment.silent(duration=0)
+    chunks = make_chunks(audio, 100)
+    ease_fn = QuadEaseIn()
+    for i, chunk in enumerate(chunks):
+        val = lerp(from_val, to_val, ease_fn.ease(i / len(chunks)))
+        out += effect(chunk, val)
+    return out
+
+def change_tempo(audio, bpm, new_bpm):
+    y = np.array(audio.get_array_of_samples())
+    if audio.channels == 2:
+        y = y.reshape((-1, 2))
+
+    sample_rate = audio.frame_rate
+
+    tempo_ratio = new_bpm / bpm
+    y_fast = pyrb.time_stretch(y, sample_rate, tempo_ratio)
+
+    channels = 2 if (y_fast.ndim == 2 and y_fast.shape[1] == 2) else 1
+    y = np.int16(y_fast * 2 ** 15)
+
+    new_seg = AudioSegment(y.tobytes(), frame_rate=sample_rate, sample_width=2, channels=channels)
+    return new_seg
 
 
 class Track:
-    def __init__(self, filename, leading_silence_ms=None):
+    def __init__(self, filename, bpm=None, leading_silence_ms=None):
         self.filename = filename
-        self.audio = AudioSegment.from_mp3(filename)
+        self.audio = AudioSegment.from_mp3(filename) - 6  # subtract some for headroom
         self.metadata = MP3(filename)
         if leading_silence_ms != None:
             self.leading_silence_ms = leading_silence_ms
         else:
             self.leading_silence_ms = self.get_leading_silence()
+        if bpm != None:
+            self.bpm = bpm
+        else:
+            self.bpm = self.get_bpm()
         self.key = self.get_key()
-        self.bpm = self.get_bpm()
         self.title = self.metadata.tags.get('TIT2')
         self.artist = self.metadata.tags.get('TPE1')
         print(f"{self.artist} - {self.title} [{self.key}, {self.bpm}, +{self.leading_silence_ms}]")
@@ -28,6 +62,13 @@ class Track:
         start_ms = self.leading_silence_ms + single_beat_ms * first
         end_ms = self.leading_silence_ms + single_beat_ms * last
         beats = self.audio[start_ms:end_ms]
+        return beats
+
+    def bars(self, first, last):
+        single_bar_ms = (60000.0 / self.bpm) * 4
+        start_ms = self.leading_silence_ms + single_bar_ms * first
+        end_ms = self.leading_silence_ms + single_bar_ms * last
+        beats = self.audio[int(start_ms):int(end_ms)]
         return beats
 
     def get_leading_silence(self):
@@ -54,20 +95,3 @@ class Track:
         new_bpm = self.metadata.tags.get('TBPM')
         return float(new_bpm.text[0])
 
-    def change_tempo(self, audiosegment, tempo, new_tempo):
-        y = np.array(audiosegment.get_array_of_samples())
-        if audiosegment.channels == 2:
-            y = y.reshape((-1, 2))
-
-        sample_rate = audiosegment.frame_rate
-
-        tempo_ratio = new_tempo / tempo
-        print(tempo_ratio)
-        y_fast = pyrb.time_stretch(y, sample_rate, tempo_ratio)
-
-        channels = 2 if (y_fast.ndim == 2 and y_fast.shape[1] == 2) else 1
-        y = np.int16(y_fast * 2 ** 15)
-
-        new_seg = AudioSegment(y.tobytes(), frame_rate=sample_rate, sample_width=2, channels=channels)
-
-        return new_seg
